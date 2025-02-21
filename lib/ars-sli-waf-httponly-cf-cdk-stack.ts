93d565b44592c2608a234ea53cbfc3ff3590757c
avinash-arora <avinash.arora@vrya.com>
2025-02-21 14:37:36 +0530

    httponly-cf-cdk

--- lib/ars-sli-waf-httponly-cf-cdk-stack.ts
+++ lib/ars-sli-waf-httponly-cf-cdk-stack.ts
@@ -1,16 +1,74 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as waf from 'aws-cdk-lib/aws-wafv2';
 
-export class ArsSliWafHttponlyCfCdkStack extends cdk.Stack {
+export class CloudFrontWafCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // WAF ACLs
+    const webAclArn = 'arn:aws:wafv2:us-east-1:610092376560:global/webacl/WebACL/c3923d94-010d-4cd1-8c6c-2e72062190b3';
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ArsSliWafHttponlyCfCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // SSL Certificate (Same for both UK and US)
+    const certificateArn = 'arn:aws:acm:us-east-1:610092376560:certificate/815d814a-facf-4fa8-8f21-52d27846b942';
+    const existingCertificate = acm.Certificate.fromCertificateArn(this, 'ExistingCertificate', certificateArn);
+
+    // UK Origin (HTTP Only)
+    const ukOrigin = new origins.HttpOrigin('svip-awslon4.sli-systems.net', {
+      protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
+      originId: "ukOrigin"
+    });
+
+    // US Origin (HTTP Only)
+    const usOrigin = new origins.HttpOrigin('svip-usa1.sli-systems.net', {
+      protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
+      originId: "usOrigin"
+    });
+
+    // UK CloudFront Distribution
+    const httpOnlyWebCloudFrontUK = new cloudfront.Distribution(this, 'HttpOnlyWebCloudFrontUK', {
+      defaultBehavior: {
+        origin: ukOrigin,
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.ALLOW_ALL,
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
+        cachePolicy: cloudfront.CachePolicy.USE_ORIGIN_CACHE_CONTROL_HEADERS_QUERY_STRINGS,
+        originRequestPolicy: cloudfront.OriginRequestPolicy.ALL_VIEWER
+      },
+      domainNames: [], // Keeping it empty as per requirement
+      certificate: existingCertificate,
+      webAclId: webAclArn,
+      comment: 'UK CloudFront Distribution',
+    });
+
+    // US CloudFront Distribution
+    const httpOnlyWebCloudFrontUS = new cloudfront.Distribution(this, 'HttpOnlyWebCloudFrontUS', {
+      defaultBehavior: {
+        origin: usOrigin,
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.ALLOW_ALL,
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
+        cachePolicy: cloudfront.CachePolicy.USE_ORIGIN_CACHE_CONTROL_HEADERS_QUERY_STRINGS,
+        originRequestPolicy: cloudfront.OriginRequestPolicy.ALL_VIEWER
+      },
+      domainNames: [], // Keeping it empty as per requirement
+      certificate: existingCertificate,
+      webAclId: webAclArn,
+      comment: 'US CloudFront Distribution',
+    });
+
+    // Outputs for UK CloudFront
+    new cdk.CfnOutput(this, 'UKCloudFrontDistributionDomain', {
+      value: httpOnlyWebCloudFrontUK.distributionDomainName,
+      description: 'The domain name of the UK CloudFront distribution',
+    });
+
+    // Outputs for US CloudFront
+    new cdk.CfnOutput(this, 'USCloudFrontDistributionDomain', {
+      value: httpOnlyWebCloudFrontUS.distributionDomainName,
+      description: 'The domain name of the US CloudFront distribution',
+    });
   }
 }
+
